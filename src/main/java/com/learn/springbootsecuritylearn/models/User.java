45dd614ca00188a01ae21e8792300b26352fbc74
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-10-30 23:52:00 +0530

    adding postgres database v10 to keep users in DB
    rather than hard coding them in configuration we are going to fetch them from DB

--- src/main/java/com/learn/springbootsecuritylearn/models/User.java
+++ src/main/java/com/learn/springbootsecuritylearn/models/User.java
@@ -1,6 +1,11 @@
 package com.learn.springbootsecuritylearn.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "saved_user")
 public class User {
+    @Id
     private String username;
     private String password;
     private String email;
